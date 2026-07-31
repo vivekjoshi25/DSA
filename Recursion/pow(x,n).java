80a93128cec27b75c3ec2afc28c6ec99577a47e1
vivek joshi <vb6274037@gmail.com>
2026-07-31 13:15:51 +0530

    solved power function using recursion

---
+++ Recursion/pow(x,n).java
@@ -0,0 +1,22 @@
+class Solution {
+    public double helper(double x, long n){
+        if(n==0){
+            return 1;
+        }
+        double half= helper(x, n/2);
+        if(n%2==0){
+            return half*half;
+        }
+        else{
+            return x *half *half;
+        }
+    }
+    public double myPow(double x, int n) {
+       long N =  n;
+       if(N<0){
+        N=-N;
+        x=1/x;
+       }
+       return helper(x, N);
+    }
+}
