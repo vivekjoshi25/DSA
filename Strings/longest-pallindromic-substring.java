325708ccb2e285a7bd42f654c55a570305d25f54
vivek joshi <vb6274037@gmail.com>
2026-09-14 20:32:55 +0530

    strings

---
+++ Strings/longest-pallindromic-substring.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String expand(String s, int left, int right){
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+        }
+        return s.substring(left+1, right);
+    }
+    public String longestPalindrome(String s) {
+        String ans="";
+        for(int i=0; i<s.length(); i++){
+            String p1= expand(s,i,i);
+            String p2=expand(s,i,i+1);
+            if(p1.length()>ans.length()){
+                ans=p1;
+
+            }
+            if(p2.length()>ans.length()){
+                ans=p2;
+            }
+
+        }
+        return ans;
+    }
+}
