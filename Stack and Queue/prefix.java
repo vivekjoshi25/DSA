b8619520bf90ba5821fae3d95d6a83d7ed5577df
vivek joshi <vb6274037@gmail.com>
2026-09-18 11:02:53 +0530

    stack and queue

---
+++ Stack and Queue/prefix.java
@@ -0,0 +1,24 @@
+class Solution {
+    static String preToPost(String s) {
+        // code here
+       
+               Stack<String> st = new Stack<>();
+
+                  for (int i = s.length()-1; i >=0; i--) {
+                      char ch = s.charAt(i);
+
+                      // Operand
+                      if (Character.isLetterOrDigit(ch)) {
+                          st.push(String.valueOf(ch));
+                      }
+                      else{
+                          String left= st.pop();
+                          String right= st.pop();
+                          String temp= left+ right+ ch;
+                          st.push(temp);
+                      }
+                  }
+
+        return st.pop();
+    }
+}
