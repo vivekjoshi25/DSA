7bcca5ad3470977b4a73ce75029e942fb977c2b6
vivek joshi <vb6274037@gmail.com>
2026-09-21 10:17:17 +0530

    stack and queue

---
+++ Stack and Queue/next-greater-element-II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+         int n=nums.length;
+         int[] ans=new int[n];
+         Arrays.fill(ans,-1);
+         Stack<Integer> st=new Stack<>();
+
+         for(int i=2*n-1; i>=0; i--){
+            int index=i%n;
+            while(!st.isEmpty() && st.peek()<=nums[index]){
+                st.pop();
+            }
+            if(i<n){
+                if(!st.isEmpty()){
+                    ans[index]=st.peek();
+                }
+            }
+            st.push(nums[index]);
+         }
+         return ans;
+    }
+}
