86f6affa7eb21316d85b429252a6c453d828f1d2
vivek joshi <vb6274037@gmail.com>
2026-09-23 19:32:50 +0530

    Arrays

---
+++ Arrays/move-zeros.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=-1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+            if(j==-1){
+                return;
+            }
+    
+            
+        for(int i=j+1; i<nums.length; i++){
+            if(nums[i]!=0){
+                int tmp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=tmp;
+                j++;
+            }
+        }
+        }
+}
